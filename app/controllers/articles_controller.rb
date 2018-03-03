820914a1c99128b1457a259dad2577d2fcababfe
amitbaraik <amitbaraik@gmail.com>
2018-03-03 22:34:10 +0530

    Edit and Show page created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+
   def new
     @article= Article.new
   end
@@ -17,12 +22,27 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article= Article.find(params[:id])
+  def edit
+    @article = Article.find(params[:id])
   end
 
-  private
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]= "The article was Updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
 
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
   def article_params
     params.require(:article).permit(:title, :description)
 
