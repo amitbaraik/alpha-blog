f94575be7e8ad204a334a92cdbb372bdd4918913
amitbaraik <amitbaraik@gmail.com>
2018-03-13 01:36:32 +0530

    edit your profile fix in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController<ApplicationController
     end
 
     def require_same_user
-      if !logged_in? & current_user!= @user
+      if current_user!= @user
         flash[:danger] ="You can edit your own account"
         redirect_to root_path
       end
