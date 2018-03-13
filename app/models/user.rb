47e51fbea2f2d2099fef5ff1f1db7e1fd05147fd
amitbaraik <amitbaraik@gmail.com>
2018-03-14 00:49:03 +0530

    Admin authentication from view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :articles
+  has_many :articles,  dependent: :destroy
   before_save { self.email = email.downcase }
 
   validates :username, presence: true,
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   length: { maximum: 105},
   uniqueness: {case_sensitive: false},
   format: { with: VALID_EMAIL_REGEXP }
-  
+
 end
