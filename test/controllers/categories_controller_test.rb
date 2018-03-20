17b64481186e6389b5da8f74da2d472f54338dc8
amitbaraik <amitbaraik@gmail.com>
2018-03-21 00:46:01 +0530

    Update Nav bar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+
+  before_action :require_admin, except:[:index, :show]
+
   def setup
     @category= Category.create(name: "sports")
+    @user= User.create(name: "john", email:"john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -11,6 +15,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@user,"password")
     get new_category_path
     assert_response :success
   end
@@ -20,5 +25,19 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "sports"}}
+    end
+    assert_redirected_to categories_path
+  end
 
+  private
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger]= "Only Admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 end
